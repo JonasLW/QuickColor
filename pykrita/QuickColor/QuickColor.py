56555f286d945f2f1f1dc4ea754e9fed9c55ab31
ghistes <ghisteslwchlohm@protonmail.com>
2019-11-17 20:31:30 +0100

    remove code duplication, add error-message

--- pykrita/QuickColor/QuickColor.py
+++ pykrita/QuickColor/QuickColor.py
@@ -1,81 +1,35 @@
-import sys
 from krita import *
+import functools
+from PyQt5.QtWidgets import QErrorMessage
 
 class QuickColorExtension(Extension):
 
-    def __init__(self, parent):
+    def __init__(self, parent, paletteName):
         #Initialising the parent, important when subclassing
         super().__init__(parent)
+        self.paletteName = paletteName
 
     def setup(self):
         pass
 
-    def swapToZero(self):
-        #Swaps the foreground color to color nr. 0 in the palette called "QuickColor"
-        resources = Application.resources("palette")
+    def swap(self, index):
         view = Krita.instance().activeWindow().activeView()
-        for (name, item) in resources.items():
-            if name == "QuickColor":
-                palette = Palette(item)
-                color = palette.colorSetEntryByIndex(0).color()
-                view.setForeGroundColor(color)
-                
-    def swapToOne(self):
-        #Swaps the foreground color to color nr. 1 in the palette called "QuickColor"
         resources = Application.resources("palette")
-        view = Krita.instance().activeWindow().activeView()
-        for (name, item) in resources.items():
-            if name == "QuickColor":
-                palette = Palette(item)
-                color = palette.colorSetEntryByIndex(1).color()
-                view.setForeGroundColor(color)
+        res = resources.get(self.paletteName, None)
+        if res is None:
+            em = QErrorMessage()
+            em.showMessage("You need a palette named \"" + self.paletteName + "\" for the QuickColorExtension to work")
+            em.exec_()
+        else:
+            palette = Palette(res)
+            color = palette.colorSetEntryByIndex(index).color()
+            view.setForeGroundColor(color)
 
-    def swapToTwo(self):
-        #Swaps the foreground color to color nr. 2 in the palette called "QuickColor"
-        resources = Application.resources("palette")
-        view = Krita.instance().activeWindow().activeView()
-        for (name, item) in resources.items():
-            if name == "QuickColor":
-                palette = Palette(item)
-                color = palette.colorSetEntryByIndex(2).color()
-                view.setForeGroundColor(color)
-
-    def swapToThree(self):
-        #Swaps the foreground color to color nr. 3 in the palette called "QuickColor"
-        resources = Application.resources("palette")
-        view = Krita.instance().activeWindow().activeView()
-        for (name, item) in resources.items():
-            if name == "QuickColor":
-                palette = Palette(item)
-                color = palette.colorSetEntryByIndex(3).color()
-                view.setForeGroundColor(color)
-                
-    def swapToFour(self):
-        #Swaps the foreground color to color nr. 4 in the palette called "QuickColor"
-        resources = Application.resources("palette")
-        view = Krita.instance().activeWindow().activeView()
-        for (name, item) in resources.items():
-            if name == "QuickColor":
-                palette = Palette(item)
-                color = palette.colorSetEntryByIndex(4).color()
-                view.setForeGroundColor(color)
-                
     def createActions(self, window):
-        action = window.createAction("QuickColor0", "QuickColor0")
-        action.triggered.connect(self.swapToZero)
-        
-        action = window.createAction("QuickColor1", "QuickColor1")
-        action.triggered.connect(self.swapToOne)
-        
-        action = window.createAction("QuickColor2", "QuickColor2")
-        action.triggered.connect(self.swapToTwo)
-        
-        action = window.createAction("QuickColor3", "QuickColor3")
-        action.triggered.connect(self.swapToThree)   
-        
-        action = window.createAction("QuickColor4", "QuickColor4")
-        action.triggered.connect(self.swapToFour)
-        
+        for n in range(4):
+            name = "QuickColor" + str(n)
+            action = window.createAction(name, name)
+            action.triggered.connect(functools.partial(self.swap, n))
 
 #Adding extension to Krita's list of extensions
-Krita.instance().addExtension(QuickColorExtension(Krita.instance()))
+Krita.instance().addExtension(QuickColorExtension(Krita.instance(), "QuickColor"))
