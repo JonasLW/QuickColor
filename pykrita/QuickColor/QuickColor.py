43a52094064d483963b095f3aa5e3ee4abd3aea2
mh <mh@mh.com>
2019-11-20 20:25:23 +0100

    read palette name from config

--- pykrita/QuickColor/QuickColor.py
+++ pykrita/QuickColor/QuickColor.py
@@ -4,22 +4,25 @@ from PyQt5.QtWidgets import QErrorMessage
 
 class QuickColorExtension(Extension):
 
-    def __init__(self, parent, paletteName):
+    def __init__(self, parent):
         #Initialising the parent, important when subclassing
         super().__init__(parent)
-        self.paletteName = paletteName
 
     def setup(self):
         pass
+
+    def paletteName(self):
+        #read name of palette from kritarc, defaulting to "QuickColor" if not found
+        return Krita.instance().readSetting("QuickColorExtension", "Palette", "QuickColor")
 
     def swap(self, index):
         #Swaps the foreground color to a color from the palette called self.paletteName, by index
         view = Krita.instance().activeWindow().activeView()
         resources = Application.resources("palette")
-        res = resources.get(self.paletteName, None)
+        res = resources.get(self.paletteName(), None)
         if res is None:
             em = QErrorMessage()
-            em.showMessage("You need a palette named \"" + self.paletteName + "\" for the QuickColorExtension to work")
+            em.showMessage(f"You need a palette named \"{self.paletteName()}\" (configurable in kritarc) for the QuickColorExtension to work.")
             em.exec_()
         else:
             palette = Palette(res)
@@ -27,10 +30,10 @@ class QuickColorExtension(Extension):
             view.setForeGroundColor(color)
 
     def createActions(self, window):
-        for n in range(4):
+        for n in range(5):
             name = "QuickColor" + str(n)
             action = window.createAction(name, name)
             action.triggered.connect(functools.partial(self.swap, n))
 
 #Adding extension to Krita's list of extensions
-Krita.instance().addExtension(QuickColorExtension(Krita.instance(), "QuickColor"))
+Krita.instance().addExtension(QuickColorExtension(Krita.instance()))
